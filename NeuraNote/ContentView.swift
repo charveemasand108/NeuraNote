86cb876349d0ef682ebe9cf7c8d3dd942da7feda
Charvee Masand <charvee.masand@gmail.com>
2026-04-23 22:01:31 +0530

    Initial Commit

---
+++ NeuraNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NeuraNote
+//
+//  Created by Charvee Masand on 23/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NeuraNote/NeuraNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuraNoteApp.swift
+//  NeuraNote
+//
+//  Created by Charvee Masand on 23/04/26.
+//
+
+import SwiftUI
+
+@main
+struct NeuraNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
